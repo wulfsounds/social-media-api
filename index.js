4d2db382d0f1fc794988161ee75244188478fd4a
Devin Wulf <devinlwulf@gmail.com>
2022-04-25 13:22:22 -0500

    Create scaffolding, dependencies, server

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import dependencies, routes
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+//PORT Set-up
+const PORT = process.env.port || 3001;
+const app = express();
+
+//Initialize Express.js with Routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+//Connecting database to PORT
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server for ${activity} running on port ${PORT}!`);
+    });
+  });
