2cdde7e440d2e3ee402aa2304d5eb0da4eb59e8b
Devin Wulf <devinlwulf@gmail.com>
2022-04-25 15:12:19 -0500

    update thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,14 +7,19 @@ const {
   deleteThought,
 } = require('../../controllers/thoughtController.js');
 
-// /api/Thoughts
+// /api/thoughts => GET all thoughts.
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/Thoughts/:ThoughtId
-router
-  .route('/:ThoughtId')
-  .get(getSingleThought)
-  .put(updateThought)
-  .delete(deleteThought);
+// /api/thoughts/:ThoughtId => GET / UPDATE / DELETE thought by ID.
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:userId => CREATE thought under User ID
+router.route('/:userId').post(createThought);
+
+// /api/thoughts/:thoughtId/reactions => CREATE reaction.
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId' => UPDATE / DELETE reaction
+router.route('/:thoughtId/reactions/:reactionId').put(updateReaction).delete(deleteReaction);
 
 module.exports = router;
